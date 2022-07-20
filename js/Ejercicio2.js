1ac91d35a7b49787d9b574d7752e5e8559d6f8b2
Alvaro <zhuone@hotmail.com>
2022-07-20 19:06:36 -0300

    completo el ejercico n2

--- js/Ejercicio2.js
+++ js/Ejercicio2.js
@@ -1,10 +1,25 @@
 let ciudades = [];
 
-do{
-    let nombreCiudades = prompt("Escribe el nombre de una ciudad")
-    for(let indice = 0; indice < nombreCiudades; indice++)
-    document.write(ciudades[indice])
 
-}while(confirm("Continue?"))
+while(listaCiudades = prompt("Introduce le nombre de una ciudad")){
+    ciudades.push(listaCiudades)
+}
+ciudades.push("Paris")
 
 
+
+ciudades.splice(1, 1, "Barcelona")
+
+document.write(`<ol>`)
+for (let i = 0; i < ciudades.length; i++){
+    document.write(`<li>${ciudades[i]}</li>`)
+}
+document.write(`</ol>`)
+
+document.write("Cantidad de ciudades ingresadas :" + ciudades.length)
+
+
+document.write("<br>La ciudad en primera posición es :" + ciudades[0])
+document.write("<br>La ciudad en segunda posición es :" + ciudades[1])
+document.write("<br>La ciudad en tercera posición es :" + ciudades[2])
+document.write("<br>La ciudad en tercera posición es : Paris")
