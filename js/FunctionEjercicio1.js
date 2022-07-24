b644670edf04bbd6cfc542903b6548a12e389ee5
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-07-23 21:11:54 -0300

    corregido y terminado el ejercicio funciones 1

--- js/FunctionEjercicio1.js
+++ js/FunctionEjercicio1.js
@@ -2,11 +2,9 @@ let numero = Number(prompt("Ingresa un número"));
 
 function parImpar() {
   if (numero % 2 == 0) {
-    document.write("El número es par ");
-  } else if (numero % 3 == 0) {
-    document.write("El número es impar ");
-  } else {
-    document.write("No ingresaste un número ");
+    document.write("El número es par");
+  } else{
+    document.write("El número es impar");
   }
 }
 parImpar(numero);
